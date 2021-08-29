a340d2d9efd9f5e3ab25382cbba53a5e647091d0
Den <83018062+DenSidorov@users.noreply.github.com>
2021-08-29 18:23:33 +0300

    Update
    models.py

--- newapp/models.py
+++ newapp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 from django.db.models import Sum
 
+
 class Author(models.Model):
     authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
     ratingAuthor = models.SmallIntegerField(default=0)
@@ -12,15 +13,18 @@ class Author(models.Model):
         pRat = 0
         pRat += postRat.get('postRating')
 
-        commentRat = self.authorUser.comment_set.aggregate(commentRating=Sum('rating'))
+        commentRat = self.authorUser.comment_set.aggregate(
+            commentRating=Sum('rating'))
         cRat = 0
         cRat += commentRat.get('commentRating')
 
         self.ratingAuthor = pRat * 3 + cRat
         self.save()
 
+
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
+
 
 class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -31,7 +35,8 @@ class Post(models.Model):
         (NEWS, 'Новость'),
         (ARTICLE, 'Статья'),
     )
-    categoryType = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+    categoryType = models.CharField(
+        max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
     dateCreation = models.DateTimeField(auto_now_add=True)
     postCategory = models.ManyToManyField(Category, through='PostCategory')
     title = models.CharField(max_length=128)
@@ -41,17 +46,19 @@ class Post(models.Model):
     def like(self):
         self.rating += 1
         self.save()
-    
+
     def dislike(self):
         self.rating -= 1
         self.save()
-    
+
     def preview(self):
-        return self.text[0:123] + '...'
+        return f'Заголовок: {self.title}\n Статья: {self.text[:124]} ...'
+
 
 class PostCategory(models.Model):
     postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
     categoryThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
+
 
 class Comment(models.Model):
     commentPost = models.ForeignKey(Post, on_delete=CASCADE)
@@ -66,7 +73,10 @@ class Comment(models.Model):
     def like(self):
         self.rating += 1
         self.save()
-    
+
     def dislike(self):
         self.rating -= 1
         self.save()
+
+    def post_com(self):
+        return f'Комментарий к статье:\n Дата: {self.dateCreation}\nПользователь: {self.commentUser}\n Рейтинг: {self.rating}\n Коментарий: {self.text}'
